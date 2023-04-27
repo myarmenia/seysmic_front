85d8345016c8803329443811e082465d695179f1
rubsargsyann <ruben.sargsyan20012018@gmail.com>
2023-04-27 13:55:13 +0400

    changes131232312

--- src/pages/monitoring/regional/blocks/MonitoringExample.js
+++ src/pages/monitoring/regional/blocks/MonitoringExample.js
@@ -6,24 +6,12 @@ export const MonitoringExample = () => {
   return (
     <Container>
       <Title>ПРИМЕР МОНИТОРИНГА</Title>
-      <div className="border w-full h-[500px] grid ">
-        <div className={styles.monitoring_box}>
-          <h2 className={styles.title}>
-            Очаговой зоны землетрясения на границе Иран-Ирак
-          </h2>
-          <div className={styles.date}>24.02.2023</div>
-          <div className={styles.tag}>
-            #теги#теги#теги#теги#теги#теги#теги#теги#
-          </div>
-          <div className={styles.img_box}>
-            <div className={styles.img}></div>
-            <div className={styles.img}></div>
-          </div>
-          <div className={styles.img_text}>
-            М=7.3 (слева) и прогноз этого землетрясения в СС Загрос по МСЭ
-            (справа) во время Презентации в ГК Росатом
-          </div>
-        </div>
+      <p>
+        Oчаговой зоны землетрясения на границе Иран-Ирак от 12 ноября 2017,
+        М=7.3 (слева) и прогноз этого землетрясения в СС Загрос по МСЭ (справа)
+        во время Презентации в ГК Росатом
+      </p>
+      <div className={styles.img_box}>
       </div>
     </Container>
   );
