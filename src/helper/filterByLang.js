6da8f4efea8fa43b45010056e11c5d3fd384e08d
Edgar <edgaravetisyan932@gmail.com>
2023-05-09 00:06:25 +0400

    changes idk 7

---
+++ src/helper/filterByLang.js
@@ -0,0 +1,20 @@
+const lang_keys = ["_am", "_ru", "_en"];
+
+export function filterByLang(arr, lang) {
+  let arr1 = JSON.parse(JSON.stringify(arr));
+  arr1 = arr1.map((el) => {
+    const myObj = {};
+    Object.entries(el).forEach(([key, value]) => {
+      if (lang_keys.includes(key.slice(-3))) {
+        if (key.slice(-2) === lang) {
+          Object.assign(myObj, { [key.slice(0, -3)]: value });
+        }
+      } else {
+        Object.assign(myObj, { [key]: value });
+      }
+    });
+    return myObj;
+  });
+
+  return arr1;
+}
