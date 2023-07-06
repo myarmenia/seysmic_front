8258fb1e40bf32d87fab333c21467eda1a839e89
Hovsep22 <avetisyan.hoso@gmail.com>
2023-07-06 12:28:35 +0400

    aaa

--- src/pages/home/blocks/Partners.js
+++ src/pages/home/blocks/Partners.js
@@ -1,8 +1,8 @@
-import React from "react";
-import logo from "../../../assets/main/LGG_logo.svg";
-import img from "../../../assets/main/home/partners_img.svg";
-import { Container, Title, ViewPosition } from "../../../components/reusable";
-import { useTranslation } from "../../../hooks";
+import React from 'react';
+import logo from '../../../assets/main/LGG_logo.svg';
+import img from '../../../assets/main/home/partners_img.svg';
+import { Container, Title, ViewPosition } from '../../../components/reusable';
+import { useTranslation } from '../../../hooks';
 
 export const Partners = () => {
   const { home: language } = useTranslation().language;
@@ -11,15 +11,13 @@ export const Partners = () => {
     <ViewPosition>
       {(bool) => (
         <Container className="flex flex-col items-center py-[65px] med-600:py-[35px]">
-          <Title className="mb-[60px] med-600:mb-[30px]">
-            {language?.strategic_partner}
-          </Title>
+          <Title className="mb-[60px] med-600:mb-[30px]">{language?.strategic_partner}</Title>
           <div className="grid grid-cols-2 gap-[50px] med-900:flex med-900:flex-col-reverse">
             <div className="flex flex-col gap-[25px] text-[#1A374D] font-bold text-lg text-justify leading-[150%] med-600:gap-4">
               <img loading="lazy" className="w-[100px]" src={logo} alt="" />
-              <p className="font-semibold text-start">
-                {language?.links_text}{" "}
-                <a className="font-bold" href="https://lgg.foundation/">
+              <p className="font-bold text-start">
+                {language?.links_text}{' '}
+                <a className="underline text-blue-600" href="https://lgg.foundation/">
                   https://lgg.foundation/
                 </a>
               </p>
@@ -31,7 +29,7 @@ export const Partners = () => {
             <img
               loading="lazy"
               style={{
-                transform: bool ? "translateX(0)" : "translateX(500px)",
+                transform: bool ? 'translateX(0)' : 'translateX(500px)',
               }}
               className="object-contain w-full duration-700"
               src={img}
